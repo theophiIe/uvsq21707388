dff32354c4a67dfef257038f8ea08c73a24866b9
theophiIe <theomolinatti@gmail.com>
2020-09-30 13:47:03 +0200

    Ajout construc + toString + exemple Main

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,12 @@
 public class Fraction {
+    private int numerateur;
+    private int denominateur;
+    public Fraction(int numerateur, int denominateur){
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+    public String toString(){
+        return "La fraction a pour denominateur " + this.denominateur + " et " + this.numerateur + " pour numerateur";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("TD 1");
+
+        var frac = new Fraction(10,5);
+        System.out.println(frac.toString());
     }
 }
